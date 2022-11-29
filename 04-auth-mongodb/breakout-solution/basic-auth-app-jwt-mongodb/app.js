ef7fbc39bb2986cb3eec0db9f40033ec798c116f
florianb <florian.bemmann@ifi.lmu.de>
2022-11-29 15:25:02 +0100

    Tutorial 4

---
+++ 04-auth-mongodb/breakout-solution/basic-auth-app-jwt-mongodb/app.js
@@ -0,0 +1,69 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const db = require('monk')('localhost/omm-2021'); // connect to database omm-2021
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(function(req,res,next){  req.db = db;
+  next();
+});
+
+
+// the login middleware. Requires BasicAuth authentication
+app.use((req,res,next) => {
+  const users = db.get('users');
+  users.findOne({basicauthtoken: req.headers.authorization}).then(user => {
+    if (user) {
+      req.username = user.username;
+      next()
+    }
+    else {
+      res.set('WWW-Authenticate', 'Basic realm="401"')
+      res.status(401).send()
+    }
+  }).catch(e => {
+    console.error(e)
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send()
+  })
+})
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
