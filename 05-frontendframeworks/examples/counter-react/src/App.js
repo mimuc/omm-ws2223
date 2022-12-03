db2701bf4c5375aebf0c053310c3ff36dc5bed63
florianb <florian.bemmann@ifi.lmu.de>
2022-12-03 09:34:05 +0100

    Tutorial 5: React js counter

---
+++ 05-frontendframeworks/examples/counter-react/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import OmmCounter from './omm-counter';
+
+function App() {
+  return (
+      <div className="app">
+        <div className="header"><h1>Counter - React</h1></div>
+        <OmmCounter />
+        {/* <Parent /> */}
+      </div>
+  );
+}
+
+export default App;
