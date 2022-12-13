37429fd57c24b95cb7da936cbc601adb77281da6
florianb <florian.bemmann@ifi.lmu.de>
2022-12-13 11:56:02 +0100

    Tutorial 6

---
+++ 06-morereact/examples/index.ts
@@ -0,0 +1,15 @@
+const x: number = 5;
+
+function joinString (x: string[]): string { return "Hallo"  }
+
+// There are    interfaces in TS
+interface Engine {
+    horsePower: number;
+    ignite (): number;
+}
+
+// Classes can   have member-visibility
+class Car {
+    private engine: Engine;
+    public start (): void {  }
+}
